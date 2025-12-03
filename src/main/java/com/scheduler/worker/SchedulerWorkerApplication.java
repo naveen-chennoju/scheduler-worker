b9db33ecdeb008d4bb84f66a1f2e3d48c5630a46
Naveen <naveenk.chennoju@gmail.com>
2025-12-02 21:13:39 -0600

    creating project skeleton

---
+++ src/main/java/com/scheduler/worker/SchedulerWorkerApplication.java
@@ -0,0 +1,12 @@
+package com.scheduler.worker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulerWorkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SchedulerWorkerApplication.class, args);
+    }
+}
